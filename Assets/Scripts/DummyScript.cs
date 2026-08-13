0d71559201d133efc59971b2b1f047890daa3c4b
th30-na3 <nicolae.theodor@yahoo.com>
2026-08-13 17:13:33 +0300

    added chestie

--- Assets/Scripts/DummyScript.cs
+++ Assets/Scripts/DummyScript.cs
@@ -5,7 +5,7 @@ public class DummyScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // add chestii haha jkjk
     }
 
     // Update is called once per frame
